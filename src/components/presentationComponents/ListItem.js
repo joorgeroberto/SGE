4af8bda2bd25b37e5253c1b430fb6fea5b40834a
Jorge Roberto <jorge@iMac-de-Jorge.local>
2019-03-30 23:59:37 -0300

    SelecionarSala, AddPlace work

---
+++ src/components/presentationComponents/ListItem.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { CardSection } from './common';
+import { Actions } from 'react-native-router-flux';
+
+class ListItem extends Component {
+    onRowPress() {
+        //Passando os dados do empregado para a proxima tela.
+        Actions.employeeEdit({ employee: this.props.employee.item });
+    }
+
+    render() {
+        const { name } = this.props.employee.item;
+
+        return (
+            <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+                <View>
+                    <CardSection>
+                        <Text style={styles.titleStyle}>
+                            {name}
+                        </Text>
+                    </CardSection>
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
+}
+
+const styles = {
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
+    }
+};
+
+export default ListItem;
